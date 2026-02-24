4871236a4f0672bd545aa89e4d65fc99ebfb96bd
portside0 <studies0and0observations@gmail.com>
2026-02-24 10:59:33 +0530

    added full names of some constants

--- constants.h
+++ constants.h
@@ -8,7 +8,7 @@ static const double e = 2.7182818284590452354;
 static const double ln2 = 0.69314718055994530942;
 static const double ln10 = 2.30258509299404568402;
 static const double sqrt2 = 1.41421356237309504880;
-static const double phi = 1.61803398875;
+static const double phi = 1.61803398875; //golden ratio
 
 /*
 Physical costants 
@@ -18,15 +18,16 @@ Physical costants
 //All constants use SI units or SI derived units
 
 static const int c = 299792458; 
-static const double h = 6.62607015E-34; 
-static const double Na = 6.02214076E23;
-static const double kb = 1.380649E-23;
-static const double echarge = 1.602176634E-19;
-static const double mu0 = 1.25663706127E-6;
-static const double ep0 = 8.8541878188E-12;
-static const double me = 9.1093837139E-31;
-static const double mp = 1.67262192525E-27;
-static const double mn = 1.67492750056E-27;
+static const double h = 6.62607015E-34; // planck's cosntant
+static const double Na = 6.02214076E23; // Avagadro number
+static const double kb = 1.380649E-23; //Boltzmann's constant
+static const double echarge = 1.602176634E-19; // elementary charge
+static const double mu0 = 1.25663706127E-6; // vacuum magnetic permeability
+static const double ep0 = 8.8541878188E-12; // vacuum electric permeability
+static const double me = 9.1093837139E-31; // mass of electron
+static const double mp = 1.67262192525E-27; // mass of proton
+static const double mn = 1.67492750056E-27; // mass of neutron
 
 #endif
+
 
